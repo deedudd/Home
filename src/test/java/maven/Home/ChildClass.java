ccae6494f9588306855ca7d758debc342f795059
Deepika <deepikamaddula85@gmail.com>
2022-10-10 09:08:38 +0530

    second commit

--- src/test/java/maven/Home/ChildClass.java
+++ src/test/java/maven/Home/ChildClass.java
@@ -1,22 +1,10 @@
 package maven.Home;
 
-public class ChildClass extends ParentClass{
-	public void method1() {
-		System.out.println("child method1");
-		
-	}
-	public static void method2() {
-		System.out.println("child method2");
-	}
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		ParentClass p = new ParentClass();
-		ChildClass c = new ChildClass();
-		
-		p.method1();
-		p.method2();
-		c.method1();
-		c.method2();
-	}
+import org.testng.annotations.Test;
 
+public class ChildClass {
+@Test
+public void test1() {
+	
+}
 }
