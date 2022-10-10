28da5c7e1e8aadaf5f8dfea91642686520e4614e
Deepika <deepikamaddula85@gmail.com>
2022-10-10 09:14:45 +0530

    second

--- src/test/java/maven/Home/ParentClass.java
+++ src/test/java/maven/Home/ParentClass.java
@@ -6,6 +6,6 @@ public class ParentClass {
 		System.out.println("parent - method1");
 	}
 	public static void method2() {
-		System.out.println("parent method2hdfjgf");
+		System.out.println("parent method2shg");
 	}
 }
