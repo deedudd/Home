1af624c9397a9264ef431b38e199ec08e4dbfd6a
Deepika <deepikamaddula85@gmail.com>
2022-10-10 15:36:03 +0530

    changed in parent in exp

--- src/test/java/maven/Home/ParentClass.java
+++ src/test/java/maven/Home/ParentClass.java
@@ -3,7 +3,7 @@ package maven.Home;
 public class ParentClass {
 
 	public void method1() {
-		System.out.println("parent1 - method1");
+		System.out.println("par - method1");
 	}
 	public static void method2() {
 	
