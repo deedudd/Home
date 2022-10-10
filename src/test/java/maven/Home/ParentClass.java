b06083868312dc9524b68f38cb5a3491f9576f95
Deepika <deepikamaddula85@gmail.com>
2022-10-10 15:46:21 +0530

    ss

--- src/test/java/maven/Home/ParentClass.java
+++ src/test/java/maven/Home/ParentClass.java
@@ -3,7 +3,7 @@ package maven.Home;
 public class ParentClass {
 
 	public void method1() {
-		System.out.println("par - method1");
+		System.out.println("para");
 	}
 	public static void method2() {
 	
